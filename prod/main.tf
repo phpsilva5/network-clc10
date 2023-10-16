131a767713082feefcf668abddd38bde901c80ef
EC2 Default User <ec2-user@ip-10-0-1-117.ec2.internal>
2023-10-16 00:55:51 +0000

    Change Env tag Value

--- prod/main.tf
+++ prod/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
     default_tags {
         tags = {
             Owner   = "Paulo Henrue P Silva"
-            Env     = "Prod"
+            Env     = "Producao"
             Class   = "CLC10"
         }
     }
